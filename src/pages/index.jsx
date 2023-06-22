2ad8da1720369ac935e227558a48a902fc6ef41b
João Cunha <joaocunhask@gmail.com>
2023-06-22 07:35:45 -0300

    landing page

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header/Header';
+import HeroSection from '../components/Hero/Hero';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+    </>
+  );
+};
+
+export default App;
